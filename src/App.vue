c0fb088a6709e516a581943f4dc8c86b067a94e2
Cameron Mockabee <cmock918@gmail.com>
2020-03-12 22:38:10 -0700

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <app-bar/>
+
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import AppBar from '@/components/AppBar'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'App',
+    components: {
+      AppBar
+    }
+  };
+
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,46 @@
+<template>
+
+	<v-app-bar
+		app
+		absolute
+		hide-on-scroll
+		>
+		
+		<v-app-bar-nav-icon></v-app-bar-nav-icon>
+		
+		<v-toolbar-title>Cameron Mockabee</v-toolbar-title>
+		
+		<v-spacer></v-spacer>
+
+
+		<v-btn
+			v-for="(button, i) in buttons"
+			:key="i"
+			:to="button.to"
+			depressed
+			text
+			min-width="100"
+			>
+		
+			{{button.name}}
+		</v-btn>
+
+	</v-app-bar>
+
+</template>
+
+
+<script>
+	export default {
+		name: 'AppBar',
+		data() {
+			return {
+				buttons: [
+					{ name: 'Home', to: '/' },
+					{ name: 'About', to: '/About' },
+					{ name: 'Contact', to: '/Contact' }  
+				]
+			}
+		}
+	};
+</script>
